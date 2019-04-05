d89348f09d2b56281ad9e8c0c4e04ceb5d620338
probe2k <kyash054@gmail.com>
2019-04-05 16:55:03 +0530

    Script to install flutter_sdk on a linux system

---
+++ flutter_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# script to install flutter_sdk on linux
+
+cd /opt
+
+echo "Downloading flutter_sdk"
+
+# using curl to download flutter_linux_v.tar.gz from flutter.dev/docs/get-started/install/linux
+
+sudo curl -LO https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.2.1-stable.tar.xz
+
+echo "Extracting flutter_sdk"
+sudo tar xvf flutter*
+
+echo "Extracted!"
+
+# clearing downloaded .tar.gz files
+sudo rm -rf flutter_linux*
+
+echo "setting ownership for the folder"
+
+sudo chown -R $USER:$USER /opt/flutter
+
+# adding /flutter/bin to PATH variable
+
+sudo echo "export PATH=${PATH}:/opt/flutter/bin"
+
+echo "Flutter_sdk is installed! Reboot shell"
