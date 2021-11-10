69e199ee20b8af6b808ec27b7eeb06f863385c1e
probe2k <kyash054@gmail.com>
2021-11-11 02:35:20 +0530

    Whatsapp now refreshing with server props
    PS : The server sync time-delay is random because they're using another cron flag for random sync, so you might have to run it every time it doesn't work instead of on backups only

--- wa_config_editor.sh
+++ wa_config_editor.sh
@@ -14,4 +14,28 @@ sed -i 's/1024/100000/g;' $file
 sed -i 's/image_processing_enabled"\ value="true"/image_processing_enabled"\ value="false"/g;' $file
 sed -i 's/55/100/g;' $file
 sed -i 's/80/100/g;' $file
+sed -i 's/75/100/g;' $file
 sed -i 's/1600/100000/g;' $file
+sed -i 's/16/1000/g;' $file
+sed -i 's/100/1000/g;' $file
+sed -i 's/32/1000/g;' $file
+
+sleep 1
+
+
+file1=/data/data/com.whatsapp/shared_prefs/server_prop_preferences.xml
+
+# string ed for major confs (particulars to be edited manually on preference)
+sed -i 's/1080/100000/g;' $file1
+sed -i 's/5000/10000/g;' $file1
+sed -i 's/1280/100000/g;' $file1
+sed -i 's/1024/100000/g;' $file1
+sed -i 's/image_processing_enabled"\ value="true"/image_processing_enabled"\ value="false"/g;' $file1
+sed -i 's/55/100/g;' $file1
+sed -i 's/80/100/g;' $file1
+sed -i 's/75/100/g;' $file1
+sed -i 's/1600/100000/g;' $file1
+sed -i 's/16/1000/g;' $file1
+sed -i 's/100/1000/g;' $file1
+sed -i 's/32/1000/g;' $file1
+
